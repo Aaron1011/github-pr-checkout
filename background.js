a8dae49a6c2925fa2b9cbee69067f8a4d64e7540
Aaron Hill <aa1ronham@gmail.com>
2015-02-15 14:04:40 -0500

    Make it really work

--- background.js
+++ background.js
@@ -1,3 +1,16 @@
+console.log("Background!");
+re = /https?:\/\/github\.com\/\w+\/\w+\/pull\/(\d+)/
+
 chrome.tabs.onUpdated.addListener(function(id, change, tab) {
-	console.log("Called: ", id, change, tab);
+    if (re.test(tab.url)) {
+        console.log("Passesd");
+        chrome.tabs.sendMessage(tab.id, {matches: true});
+    } else {
+        console.log("Failed");
+        chrome.tabs.sendMessage(tab.id, {matches: false});
+    }
+	/*console.log("Called3:  " + id + tab.url);
+	console.log("Id: " + id.toString());
+        //console.log("Change: ", change.toString());
+        console.log("Tab josn: ", JSON.stringify(tab));*/
 });
